4361da6771bd77fea0ee48ae8a920868b6aabf77
Dhruv Dhar <dhruvdhar1@gmail.com>
2023-04-25 20:59:27 -0400

    added inventory service methods to gateway interface

--- src/gateway/EcomInterfaceImpl.java
+++ src/gateway/EcomInterfaceImpl.java
@@ -21,4 +21,24 @@ public class EcomInterfaceImpl extends UnicastRemoteObject implements EcomInterf
   public void addProduct(Product product) throws RemoteException {
     LoadBalancer.getInventoryService().addProduct(product);
   }
+
+  @Override
+  public Product getProductInfo(int productId) throws RemoteException {
+    return LoadBalancer.getInventoryService().getProductInfo(productId);
+  }
+
+  @Override
+  public void updateProductInfo(Product product) throws RemoteException {
+    LoadBalancer.getInventoryService().updateProductInfo(product);
+  }
+
+  @Override
+  public int getProductStock(int prodId) throws RemoteException {
+    return LoadBalancer.getInventoryService().getProductStock(prodId);
+  }
+
+  @Override
+  public void updateProductStock(int productId, int stockVal) throws RemoteException {
+    LoadBalancer.getInventoryService().updateProductStock(productId, stockVal);
+  }
 }
